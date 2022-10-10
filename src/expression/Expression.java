6100a18f39bd07c5dfdd47fbbf865e5a4f589d3d
y62 <y62@outlook.dk>
2022-10-10 12:25:27 +0200

    new

--- src/expression/Expression.java
+++ src/expression/Expression.java
@@ -39,28 +39,3 @@ class Simulate {
 class Update {
 }
 
-class Variable extends Expression {
-    String id;
-
-    public Variable(String id) {
-        this.id =  id;
-    }
-
-    @Override
-    public String toString() {
-        return id;
-    }
-}
-
-class VariableDec extends Expression {
-    public String id;
-    public String type;
-    public int value;
-
-    public void VariableDeclaration(String id, String type, int value) {
-        this.id = id;
-        this.type = type;
-        this.value = value;
-    }
-}
-
